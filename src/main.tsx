facd7d5aacb38584581c12ac6a356a8eb6565fba
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-06 21:56:06 +0900

    chore: tanstack-query 설치 & 셋업

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import Drilling from '@pages/drilling';
 import RootComponent from './RootComponent.tsx';
 import Exam from '@pages/exam';
 import { getSeries } from './apis/series.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -31,6 +40,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
