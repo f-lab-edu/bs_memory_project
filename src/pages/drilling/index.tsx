3e4969db7819dbfa1a4d7ee815d48c6ac96e24c6
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-14 17:15:05 +0900

    feat: drilling page에 nav, 옵션영역 추가

--- src/pages/drilling/index.tsx
+++ src/pages/drilling/index.tsx
@@ -1,5 +1,27 @@
-function Drilling() {
-  return <div>drilling</div>;
+import { Link } from 'react-router-dom';
+import { FaHome } from '@react-icons/all-files/fa/FaHome';
+import Drilling from '@features/drilling';
+
+function DrillingPage() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <nav className='mb-2 mt-4 flex w-full items-center justify-start'>
+        <ul className='flex items-center justify-center space-x-4'>
+          <li className='flex w-[100px] items-center rounded-3xl bg-secondary text-center text-3xl text-white mobile:text-base'>
+            <Link to={`/`} className='inline-block w-full px-4 py-2.5'>
+              <FaHome className='w-full' />
+            </Link>
+          </li>
+          <li className='flex w-[150px] items-center rounded-3xl bg-secondary text-center text-2xl text-white mobile:text-base'>
+            <Link to={`/exam`} className='inline-block w-full px-4 py-2.5'>
+              시험보기
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Drilling />
+    </div>
+  );
 }
 
-export default Drilling;
+export default DrillingPage;
