e257d2555c852ff28bdab81c290b8140c392896b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-15 21:45:17 +0900

    feat: 암송구절 상세조회쿼리 수정

--- src/apis/custom.types.ts
+++ src/apis/custom.types.ts
@@ -3,13 +3,14 @@ import { Tables } from './database.types.ts';
 export type SeriesCode = Tables<'series'>['series_code'];
 
 export type BibleVersion = Tables<'bible_version'>;
-export type BibleVersionName = BibleVersion['name'];
 
 export type CardSortMethod = Tables<'card_sort_method'>;
 
 export type CardHideOption = Tables<'card_hide_option'>;
 
-export type ApiResult<T extends (args?: any) => Promise<any>> = Awaited<
+export type Verse = Tables<'verse'>;
+
+export type ApiResult<T extends (...args: any) => Promise<any>> = Awaited<
   ReturnType<T>
 >;
 
