a2e2aaba7647c85f4b85f46bd088365dd73b1bde
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-17 17:47:51 +0900

    feat: 암송/시험페이지 마운트시 선택구절 없으면 메인페이지로 이동

--- src/pages/drilling/index.tsx
+++ src/pages/drilling/index.tsx
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaHome } from '@react-icons/all-files/fa/FaHome';
 import Drilling from '@features/drilling';
+import { useVerseSelectStore } from '@store/verseSelectStore.ts';
+import { useEffect } from 'react';
 
 function DrillingPage() {
+  const navigate = useNavigate();
+  const hasSelectedVerse = useVerseSelectStore(state => state.hasAnyId);
+
+  useEffect(() => {
+    if (!hasSelectedVerse()) void navigate('/', { replace: true });
+  }, [hasSelectedVerse, navigate]);
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <nav className='mb-2 mt-4 flex w-full items-center justify-start'>
         <ul className='flex h-[80px] items-center justify-center space-x-4'>
           <li className='flex w-[100px] items-center rounded-3xl bg-secondary text-center text-3xl text-white mobile:text-base'>
             <Link to={`/`} className='inline-block w-full px-4 py-2.5'>
-              <FaHome className='w-full' />
+              <FaHome aria-hidden={true} className='w-full' />
+              <span className='sr-only'>홈으로</span>
             </Link>
           </li>
           <li className='flex w-[150px] items-center rounded-3xl bg-secondary text-center text-2xl text-white mobile:text-base'>
@@ -19,7 +29,7 @@ function DrillingPage() {
           </li>
         </ul>
       </nav>
-      <Drilling />
+      {hasSelectedVerse() && <Drilling />}
     </div>
   );
 }
---
+++ src/pages/exam/index.test.tsx
@@ -0,0 +1,5 @@
+import { describe, test } from 'vitest';
+
+describe('Exam Page Test', () => {
+  test('if no selected verses on page reload, it redirects to "/" ');
+});
