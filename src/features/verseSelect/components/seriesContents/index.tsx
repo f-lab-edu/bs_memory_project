d36c06001626e6dc640468b942735aa63c17d7b4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-03-19 21:15:25 +0900

    feat: 메인페이지 SeriesTab ErrorBoundary 적용

--- src/features/verseSelect/components/seriesContents/index.tsx
+++ src/features/verseSelect/components/seriesContents/index.tsx
@@ -3,9 +3,15 @@ import { SeriesDatum } from '@features/verseSelect/types';
 import { Suspense, useEffect, useRef } from 'react';
 import SubSeriesTabs from '@features/verseSelect/components/subSeriesTabs';
 import Loader from '@components/Loader';
-import { usePrefetchQuery } from '@tanstack/react-query';
+import {
+  usePrefetchQuery,
+  useQueryErrorResetBoundary,
+} from '@tanstack/react-query';
 import { getSubSeries } from '@apis/series';
 import { getVersesSummary } from '@apis/verse';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorMessage from '@components/ErrorMessage';
+import { CgRedo } from '@react-icons/all-files/cg/CgRedo';
 
 export type SeriesContentsProps = {
   data: SeriesDatum;
@@ -14,6 +20,7 @@ export type SeriesContentsProps = {
 };
 
 function SeriesContents({ data, contentsId, isTabOpen }: SeriesContentsProps) {
+  const { reset: handleQueryErrorReset } = useQueryErrorResetBoundary();
   const tabpanelRef = useRef<HTMLDivElement>(null);
 
   const { sub_series_opt, series_code } = data;
@@ -48,13 +55,28 @@ function SeriesContents({ data, contentsId, isTabOpen }: SeriesContentsProps) {
       className='scroll-mb-[50px] scroll-mt-[100px]'
     >
       {isTabOpen && (
-        <Suspense fallback={<Loader />}>
-          {hasSubSeries ? (
-            <SubSeriesTabs parent_series_code={series_code} />
-          ) : (
-            <VerseSelector series_code={series_code} />
+        <ErrorBoundary
+          onReset={handleQueryErrorReset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <ErrorMessage className='mt-3 pl-4'>
+              <div className='flex w-full items-center space-x-3'>
+                <button title='재시도' onClick={() => resetErrorBoundary()}>
+                  <span className='sr-only'>재시도</span>
+                  <CgRedo aria-hidden={true} className='size-7' />
+                </button>
+                <span>데이터 조회에 실패했습니다.</span>
+              </div>
+            </ErrorMessage>
           )}
-        </Suspense>
+        >
+          <Suspense fallback={<Loader />}>
+            {hasSubSeries ? (
+              <SubSeriesTabs parent_series_code={series_code} />
+            ) : (
+              <VerseSelector series_code={series_code} />
+            )}
+          </Suspense>
+        </ErrorBoundary>
       )}
     </div>
   );
