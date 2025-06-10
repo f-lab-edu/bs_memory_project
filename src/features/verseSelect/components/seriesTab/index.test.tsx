8d222dc75708f9e7e975d12c8998de9f9255723c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2025-06-10 20:46:42 +0900

    fix: SeriesTab 테스트코드 주석 해제

--- src/features/verseSelect/components/seriesTab/index.test.tsx
+++ src/features/verseSelect/components/seriesTab/index.test.tsx
@@ -1,99 +1,117 @@
-import { describe, test } from 'vitest';
+import {
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+  vi,
+} from 'vitest';
+import { cleanup, screen, within } from '@testing-library/react';
+import { UserEvent, userEvent } from '@testing-library/user-event';
+import SeriesTab from '@features/verseSelect/components/seriesTab/index';
+import { render } from '@/test/test-utils';
+import {
+  SERIES_DATA,
+  SERIES_DATA_HAS_SUB,
+  SERIES_DATA_NO_SUB,
+  VERSE_SUMMARY_DATA,
+} from '@/mock/mockData';
 
 describe('SeriesTab Test', () => {
-  // let user: UserEvent;
-  //
-  // beforeAll(() => {
-  //   window.HTMLElement.prototype.scrollIntoView = vi.fn();
-  // });
-  //
-  // beforeEach(() => {
-  //   user = userEvent.setup();
-  //   const seriesTabs = SERIES_DATA.map(data => (
-  //     <SeriesTab key={data.series_code} data={data} />
-  //   ));
-  //   render(<div role='tabList'>{seriesTabs}</div>);
-  // });
-  //
-  // afterEach(() => {
-  //   cleanup();
-  // });
+  let user: UserEvent;
+
+  beforeAll(() => {
+    window.HTMLElement.prototype.scrollIntoView = vi.fn();
+  });
+
+  beforeEach(() => {
+    user = userEvent.setup();
+    const seriesTabs = SERIES_DATA.map(data => (
+      <SeriesTab key={data.series_code} data={data} />
+    ));
+    render(<div role='tabList'>{seriesTabs}</div>);
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
 
   test('when clicks a series tab, associated tabpanel opens', async () => {
-    // const testTab = screen.getByRole('tab', {
-    //   name: SERIES_DATA[0].series_name,
-    // });
-    //
-    // expect(testTab.ariaExpanded).toBe('false');
-    //
-    // await user.click(testTab);
-    //
-    // expect(testTab.ariaExpanded).toBe('true');
-    //
-    // const panels = await screen.findAllByRole('tabpanel');
-    // const testPanel = panels.find(
-    //   element => element.id === testTab.getAttribute('aria-controls'),
-    // )!;
-    // expect(testPanel).toBeDefined();
-    // expect(testPanel.hidden).toBe(false);
+    const testTab = screen.getByRole('tab', {
+      name: SERIES_DATA[0].series_name,
+    });
+
+    expect(testTab.ariaExpanded).toBe('false');
+
+    await user.click(testTab);
+
+    expect(testTab.ariaExpanded).toBe('true');
+
+    const panels = await screen.findAllByRole('tabpanel');
+    const testPanel = panels.find(
+      element => element.id === testTab.getAttribute('aria-controls'),
+    )!;
+    expect(testPanel).toBeDefined();
+    expect(testPanel.hidden).toBe(false);
   });
 
   test('when clicks a multi-depth series tab, associated tabpanel has child tabs', async () => {
-    // const testTab = await screen.findByRole('tab', {
-    //   name: SERIES_DATA_HAS_SUB.series_name,
-    // });
-    //
-    // expect(testTab.ariaExpanded).toBe('false');
-    //
-    // await user.click(testTab);
-    //
-    // expect(testTab.ariaExpanded).toBe('true');
-    //
-    // const panels = await screen.findAllByRole('tabpanel');
-    // const testPanel = panels.find(
-    //   element => element.id === testTab.getAttribute('aria-controls'),
-    // )!;
-    //
-    // expect(testPanel).toBeDefined();
-    // expect(testPanel.hidden).toBe(false);
-    //
-    // const childTabs = await within(testPanel).findAllByRole('tab');
-    // childTabs.forEach(element => expect(element.ariaExpanded).toBe('false'));
+    const testTab = await screen.findByRole('tab', {
+      name: SERIES_DATA_HAS_SUB.series_name,
+    });
+
+    expect(testTab.ariaExpanded).toBe('false');
+
+    await user.click(testTab);
+
+    expect(testTab.ariaExpanded).toBe('true');
+
+    const panels = await screen.findAllByRole('tabpanel');
+    const testPanel = panels.find(
+      element => element.id === testTab.getAttribute('aria-controls'),
+    )!;
+
+    expect(testPanel).toBeDefined();
+    expect(testPanel.hidden).toBe(false);
+
+    const childTabs = await within(testPanel).findAllByRole('tab');
+    childTabs.forEach(element => expect(element.ariaExpanded).toBe('false'));
   });
 
   test('when clicks a single-depth series tab, associated tabpanel has verse checkboxes', async () => {
-    // const testTab = await screen.findByRole('tab', {
-    //   name: SERIES_DATA_NO_SUB.series_name,
-    // });
-    //
-    // expect(testTab.ariaExpanded).toBe('false');
-    //
-    // await user.click(testTab);
-    //
-    // expect(testTab.ariaExpanded).toBe('true');
-    //
-    // const panels = await screen.findAllByRole('tabpanel');
-    // const testPanel = panels.find(
-    //   element => element.id === testTab.getAttribute('aria-controls'),
-    // )!;
-    //
-    // expect(testPanel).toBeDefined();
-    // expect(testPanel.hidden).toBe(false);
-    //
-    // for (const data of VERSE_SUMMARY_DATA) {
-    //   const {
-    //     theme,
-    //     bible_code: { bible_name },
-    //     chapter,
-    //     verse1,
-    //     verse2,
-    //   } = data;
-    //
-    //   const name = `${theme} ${bible_name} ${chapter}:${verse2 ? verse1 + '-' + verse2 : verse1}`;
-    //
-    //   await expect(
-    //     within(testPanel).findByRole('checkbox', { name, checked: false }),
-    //   ).resolves.not.toThrowError();
-    // }
+    const testTab = await screen.findByRole('tab', {
+      name: SERIES_DATA_NO_SUB.series_name,
+    });
+
+    expect(testTab.ariaExpanded).toBe('false');
+
+    await user.click(testTab);
+
+    expect(testTab.ariaExpanded).toBe('true');
+
+    const panels = await screen.findAllByRole('tabpanel');
+    const testPanel = panels.find(
+      element => element.id === testTab.getAttribute('aria-controls'),
+    )!;
+
+    expect(testPanel).toBeDefined();
+    expect(testPanel.hidden).toBe(false);
+
+    for (const data of VERSE_SUMMARY_DATA) {
+      const {
+        theme,
+        bible_code: { bible_name },
+        chapter,
+        verse1,
+        verse2,
+      } = data;
+
+      const name = `${theme} ${bible_name} ${chapter}:${verse2 ? verse1 + '-' + verse2 : verse1}`;
+
+      await expect(
+        within(testPanel).findByRole('checkbox', { name, checked: false }),
+      ).resolves.not.toThrowError();
+    }
   });
 });
